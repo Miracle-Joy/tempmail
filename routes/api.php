dbf6ec9b1974f7ad5aac32156e3c2a1732d60496
Miracle-Joy <servtailscale1@gmail.com>
2026-04-10 12:46:46 -0600

    Subida de TMail v7.2

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\APIController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
+use App\Models\Message;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('cron/{password}', [AppController::class, 'cron']);
+
+Route::post('atlas/receive', function (Request $request) {
+    Message::store($request);
+});
+
+Route::get('domains/{key}', [APIController::class, 'domains']);
+Route::get('email/{email}/{key}', [APIController::class, 'email']);
+Route::get('messages/{email}/{key}', [APIController::class, 'messages']);
+Route::delete('message/{message_id}/{key}', [APIController::class, 'delete']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
